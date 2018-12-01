1e0e210b3f9372b1d03e736c9db6185c09e08183
SergeyMuratkin <SergeyMuratkin>
2018-12-01 14:18:23 +0400

    throw

--- src/robots2/RobotImprovedManager.java
+++ src/robots2/RobotImprovedManager.java
@@ -1,8 +1,13 @@
 package robots2;
 
+import java.util.Random;
+
 public class RobotImprovedManager implements RobotConnectionManager {
     @Override
     public RobotConnection getConnection() {
-        return null;
+        Random r = new Random();
+        boolean b = r.nextBoolean();
+        if (b) throw new RobotConnectionException("fs");
+        return new RobotImproved();
     }
 }
