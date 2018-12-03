e22d2daa05d16657cdf9560c89b30289ebda136a
SergeyMuratkin <SergeyMuratkin>
2018-12-03 15:58:05 +0400

    1 try

--- src/robots2/RobotController.java
+++ src/robots2/RobotController.java
@@ -2,22 +2,20 @@ package robots2;
 
 class RobotController {
     static void moveRobot(RobotConnectionManager robotConnectionManager, int toX, int toY) {
-        RobotConnection robotImproved;
+        RobotConnection robotImproved = null;
         for (int i = 3; i > 0; i--) {
             try {
                 robotImproved = robotConnectionManager.getConnection();
-                try {
-                    robotImproved.moveRobotTo(toX, toY);
-                    break;
-                } catch (RobotConnectionException e) {
-                    robotImproved.close();
-                    throw new RobotConnectionException("ошибка попытки управления", e);
-                } finally {
-                    robotImproved.close();
-                }
+                robotImproved.moveRobotTo(toX, toY);
+                robotImproved.close();
+                break;
             } catch (RobotConnectionException e) {
                 System.out.println(i);
                 if (i == 1) throw new RobotConnectionException("соединение неудачно", e);
+            } catch (Exception e) {
+                throw new RobotConnectionException("соединение неудачно", e);
+            } finally {
+                if (robotImproved != null) robotImproved.close();
             }
         }
     }
