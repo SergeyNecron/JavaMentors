e654954245e8c831c2c5740a86d871e5dc8787ff
SergeyMuratkin <SergeyMuratkin>
2018-12-06 19:46:20 +0400

    1 for
    Signed-off-by: SergeyMuratkin <SergeyMuratkin>

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -14,11 +14,9 @@ public class Main {
             ArrayList<String> list = new ArrayList<>();
             list.addAll(Arrays.asList(s.split(" ")));
 
-            for (int i = 0; i < list.size(); i++) {
-                list.remove(i);
-            }
             for (int i = list.size() - 1; i >= 0; i--) {
-                System.out.print(list.get(i) + " ");
+                list.remove(i);
+                if (i > 0) System.out.print(list.get(--i) + " ");
             }
         }
     }
