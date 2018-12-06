8a11ef37dc04ef04304f78034da31001903a0fc4
SergeyMuratkin <sergeymuratkin>
2018-12-06 19:45:46 +0400

    if
    Signed-off-by: SergeyMuratkin <SergeyMuratkin>

--- src/Collections/Main.java
+++ src/Collections/Main.java
@@ -10,14 +10,16 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
-        ArrayList<String> list = new ArrayList<>();
-        list.addAll(Arrays.asList(s.split(" ")));
+        if (s != null) {
+            ArrayList<String> list = new ArrayList<>();
+            list.addAll(Arrays.asList(s.split(" ")));
 
-        for (int i = 0; i < list.size(); i++) {
-            list.remove(i);
-        }
-        for (int i = list.size() - 1; i >= 0; i--) {
-            System.out.print(list.get(i) + " ");
+            for (int i = 0; i < list.size(); i++) {
+                list.remove(i);
+            }
+            for (int i = list.size() - 1; i >= 0; i--) {
+                System.out.print(list.get(i) + " ");
+            }
         }
     }
 }
